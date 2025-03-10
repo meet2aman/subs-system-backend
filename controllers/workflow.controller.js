a2a1145d4fb2c099abb7fe9f79eeafd2906b574a
meet2aman <meet2amankushwaha@gmail.com>
2025-03-11 00:32:41 +0530

    SMTP Mailer setup complete

--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -3,6 +3,7 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const { serve } = require("@upstash/workflow/express");
 import Subscription from "../models/subscription.model.js";
+import { sendReminderEmail } from "../utils/send-email.js";
 
 const REMINDERS = [7, 5, 2, 1];
 
@@ -31,7 +32,7 @@ export const sendReminders = serve(async (context) => {
         reminderDate
       );
     }
- 
+
     if (dayjs().isSame(reminderDate, "day")) {
       await triggerReminder(
         context,
@@ -53,14 +54,14 @@ const sleepUntilReminder = async (context, label, date) => {
   await context.sleepUntil(label, date.toDate());
 };
 
-const triggerReminder = async (context, label) => {
+const triggerReminder = async (context, label, subscription) => {
   return await context.run(label, async () => {
     console.log(`Triggering ${label} reminder`);
 
-    // await sendReminderEmail({
-    //   to: subscription.user.email,
-    //   type: label,
-    //   subscription,
-    // });
+    await sendReminderEmail({
+      to: subscription.user.email,
+      type: label,
+      subscription,
+    });
   });
 };
